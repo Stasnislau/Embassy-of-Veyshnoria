16b0b62056798dd554386a55f4f94157d2beefa7
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-13 23:11:21 +0100

    code refactoring for login page

--- Client/src/Pages/LoginPage/index.tsx
+++ Client/src/Pages/LoginPage/index.tsx
@@ -1,9 +1,13 @@
 import "./index.scss";
 
+import * as Yup from "yup";
+
+import { ErrorMessage, Field, Form, Formik } from "formik";
+
 import { Context } from "../../index";
-import ErrorMenu from "../../Components/ErrorMenu";
+import ErrorModal from "../../Components/ErrorModal";
 import React from "react";
-import { useFormik } from "formik";
+import TextError from "../../Components/TextError";
 import { useNavigate } from "react-router-dom";
 
 interface values {
@@ -19,85 +23,76 @@ const LoginPage = () => {
     email: "",
     password: "",
   };
-
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email format").required("Required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Required"),
+  });
   const onSubmit = (values: values) => {
     store.login(values.email, values.password);
-    
+
     if (!store.isAuthorized) {
       setIsErrorOpen(true);
       return;
     }
     navigate("/dashboard");
   };
-  const validate = (values: values) => {
-    let errors: Partial<values> = {};
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-    if (!values.password) {
-      errors.password = "Required";
-    } else if (values.password.length < 8) {
-      errors.password = "Password must be at least 8 characters";
-    }
-    return errors;
-  };
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validate,
-  });
+
   return (
     <div className="login-page-container">
       <div className="login-form-container">
         <h3>Please log in into the system </h3>
-        <form onSubmit={formik.handleSubmit}>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input
-              className="input-field"
-              type="email"
-              name="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              onBlur={formik.handleBlur}
-              placeholder="example@gmail.com"
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="error">{formik.errors.email}</div>
-            ) : null}
-          </div>
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input
-              className="input-field"
-              type="password"
-              name="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              onBlur={formik.handleBlur}
-              placeholder="********"
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <div className="error">{formik.errors.password}</div>
-            ) : null}
-          </div>
-          <div className="submit-button-container">
-            <button className="submit-button" type="submit">
-              Login
-            </button>
-          </div>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form>
+            <div className="login-form">
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <Field
+                  className="input-field"
+                  type="email"
+                  name="email"
+                  placeholder="example@gmail.com"
+                />
+                <ErrorMessage name="email" component={TextError} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <Field
+                  className="input-field"
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                />
+                <ErrorMessage name="password" component={TextError} />
+              </div>
+              <div className="submit-button-container">
+                <button className="submit-button" type="submit">
+                  Login
+                </button>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+        <div className="additional-login-container">
           <hr />
           <div className="links-container">
             <a href="/forgot-password">Forgot password?</a>
             <a href="/create-account">Create account</a>
           </div>
-        </form>
+        </div>
       </div>
-      {isErrorOpen && <ErrorMenu message="Incorrect Credentials" />}
+      {isErrorOpen && (
+        <ErrorModal
+          message={"Incorrect Credentials"}
+          handleOkay={() => setIsErrorOpen(false)}
+          open={isErrorOpen}
+        />
+      )}
     </div>
   );
 };
