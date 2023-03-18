6dc9e02a0d3dbd9920f51721f4319a505316111c
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-19 00:56:38 +0100

    changed NothingFoundComponent

--- Client/src/Components/NoEventsCard/index.tsx
+++ Client/src/Components/NoEventsCard/index.tsx
@@ -2,13 +2,16 @@ import "./index.scss";
 
 import React from "react";
 
-const NoEventsCard = () => {
+const NoEventsCard: React.FC = () => {
   return (
-    <div className="no-events-card">
-      <h1 className="no-events-text">You have no events {':('}</h1>
-      <h1 className="no-events-text">
-        You should add something to be able to see it here
-      </h1>
+    <div className="events-to-see">
+      <div className="events-to-see__message">
+        <h2 className="events-to-see__title">You don't have any events yet</h2>
+        <p className="events-to-see__description">
+          Start creating events to see them here!
+        </p>
+      </div>
+      <div className="events-to-see__image"></div>
     </div>
   );
 };
