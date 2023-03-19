9136275d5def05ca595dc32db17fd65df1d45b58
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-19 19:04:08 +0100

    added loading component

---
+++ Client/src/Components/LoadingComponent/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+const LoadingComponent = () => {
+  return (
+    <div className="loading-component">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingComponent;
