bf47c613f516c55e7aba3f4bbdb9ba38282de96f
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-26 22:59:35 +0200

    updated naming

---
+++ src/Dtos/user-dto.ts
@@ -0,0 +1,10 @@
+class UserDto {
+  id: number;
+  email: string;
+  constructor(user: { id: number; email: string }) {
+    this.id = user.id;
+    this.email = user.email;
+  }
+};
+
+export default UserDto;
