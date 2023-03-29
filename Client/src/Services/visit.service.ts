a05a0ab0d257e3f5bac2e1ec21e1048bf734110d
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-03-30 00:24:31 +0200

    updated routs

--- Client/src/Services/visit.service.ts
+++ Client/src/Services/visit.service.ts
@@ -1,5 +1,6 @@
 import { VisitFrontInterface, VisitInterface } from "../Interfaces";
 
+import { API_URL } from "../Http";
 import { AxiosResponse } from "axios";
 import api from "../Http";
 
@@ -10,31 +11,31 @@ const headers = {
 
 class VisitService {
   static async fetchVisitsByUser(): Promise<AxiosResponse<VisitInterface[]>> {
-    return api.get<VisitInterface[]>("/visits/users", { headers });
+    return api.get<VisitInterface[]>(`${API_URL}/visits/users`, { headers });
   }
 
   static async fetchVisitById(
     id: string
   ): Promise<AxiosResponse<VisitInterface>> {
-    return api.get<VisitInterface>(`/visits/specific/${id}`, { headers });
+    return api.get<VisitInterface>(`${API_URL}/visits/specific/${id}`, { headers });
   }
 
   static async createVisit(
     visit: VisitFrontInterface
   ): Promise<AxiosResponse<VisitFrontInterface>> {
-    return api.post<VisitFrontInterface>("/visits/create", visit, { headers });
+    return api.post<VisitFrontInterface>(`${API_URL}/visits/create`, visit, { headers });
   }
 
   static async updateVisit(
     visit: VisitInterface
   ): Promise<AxiosResponse<VisitInterface>> {
-    return api.put<VisitInterface>(`/visits/update/${visit.id}`, visit, {
+    return api.put<VisitInterface>(`${API_URL}/visits/update/${visit.id}`, visit, {
       headers,
     });
   }
 
   static async deleteVisit(id: string): Promise<AxiosResponse<VisitInterface>> {
-    return api.delete<VisitInterface>(`/visits/delete/${id}`, { headers });
+    return api.delete<VisitInterface>(`${API_URL}/visits/delete/${id}`, { headers });
   }
 }
 
