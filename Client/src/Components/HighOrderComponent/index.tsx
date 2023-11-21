dc09d8281f75019fb7747daf9901f47282dc11e5
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-11-21 17:56:55 +0100

    added high order component

---
+++ Client/src/Components/HighOrderComponent/index.tsx
@@ -0,0 +1,24 @@
+import React, { useContext, } from 'react';
+import { Context } from '../..';
+import './index.scss';
+
+
+const WrapperComponent = ({ children }: {
+    children: React.ReactNode
+}) => {
+    const { store } = useContext(Context);
+    return (
+        <div className="wrapper">
+            {store.isLoading &&
+                <div className='loader-container'>
+                    <span className='loader' />
+                </div>
+            }
+            <div className="wrapper-container">
+                {children}
+            </div>
+        </div>
+    )
+
+}
+export default WrapperComponent;
